046c65d22516658d0342c05c181da7d45d16fcc7
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2024-10-02 23:54:15 +0300

    add comments for controller

--- CardsServer.API/Controllers/ModuleController.cs
+++ CardsServer.API/Controllers/ModuleController.cs
@@ -7,24 +7,44 @@ namespace CardsServer.API.Controllers
     /// </summary>
     public class ModuleController : ControllerBase
     {
+        /// <summary>
+        /// Метод создания модулей
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         [HttpPost("module")]
         public async Task<IActionResult> CreateModule(CancellationToken cancellationToken)
         {
             return Ok();
         }
 
+        /// <summary>
+        /// Метод получения модулей
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         [HttpGet("module")]
         public async Task<IActionResult> GetModule(CancellationToken cancellationToken)
         {
             return Ok();
         }
 
+        /// <summary>
+        /// Метод редактирования модулей
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         [HttpPut("module")]
         public async Task<IActionResult> EditModule(CancellationToken cancellationToken)
         {
             return Ok();
         }
 
+        /// <summary>
+        /// Метод удаления модулей
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         [HttpDelete("module")]
         public async Task<IActionResult> DeleteModule(CancellationToken cancellationToken)
         {
