b15bf616169756ecc956c81ae051e2ae9b09d2d1
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2024-11-06 01:16:09 +0300

    feat: вкатываемся в gRPC

---
+++ CardsServer.BLL/Abstractions/IAnalyticService.cs
@@ -0,0 +1,8 @@
+﻿
+namespace CardsServer.BLL.Abstractions
+{
+    public interface IAnalyticService
+    {
+        Task<AnalyticsResponse> SendTestDataAsync(AnalyticsRequest request);
+    }
+}
---
+++ CardsServer.BLL/Services/Analytic/AnalyticService.cs
@@ -0,0 +1,35 @@
+﻿using CardsServer.BLL.Abstractions;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
+
+namespace CardsServer.BLL.Services.Analytic
+{
+    public class AnalyticService : IAnalyticService
+    {
+        //private readonly DataAnalytic.DataAnalyticClient _grpcClient;
+        private readonly GrpcChannel _grpcChannel;
+
+        public AnalyticService(GrpcChannel grpcChannel)
+        {
+            _grpcChannel = grpcChannel;
+        }
+
+        public async Task<AnalyticsResponse> SendTestDataAsync(AnalyticsRequest request)
+        {
+            //using var channel = GrpcChannel.ForAddress("http://analytic-service:8080");
+            var client = new AnalyticService(_grpcChannel);
+
+            // Создаем запрос
+            var rq = new AnalyticsRequest { Id = 123 };
+
+            // Отправляем запрос
+            AnalyticsResponse response = await client.SendTestDataAsync(request);
+
+            // Выводим результат
+            Console.WriteLine($"Result: {response.Result}");
+            Console.WriteLine($"Details: {response.Details}");
+
+            return response;
+        }
+    }
+}
