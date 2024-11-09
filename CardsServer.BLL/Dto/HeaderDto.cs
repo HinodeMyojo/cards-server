dc3688bd3033040b2da9d639e1b8d95694b96a2a
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2024-11-09 23:03:09 +0300

    feat: добавлен метод, возвращающий настройки таблицы. Исправлены мелкие недочеты

---
+++ CardsServer.BLL/Dto/HeaderDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CardsServer.BLL.Dto
+{
+    public class HeaderDto
+    {
+        public required string Title { get; set; }
+        public bool Sortable { get; set;}
+        public required string Key { get; set;}
+    }
+}
