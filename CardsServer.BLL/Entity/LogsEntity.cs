2352a888188a1e9bb00ba6bda65ed095b18dc731
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2024-12-31 01:04:28 +0300

    faet: добавлена сущность и миграция на основании этой сущности в БД

---
+++ CardsServer.BLL/Entity/LogsEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace CardsServer.BLL.Entity
+{
+    public class LogsEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreateAt {  get; set; }
+        public required string Message { get; set; }
+    }
+}
