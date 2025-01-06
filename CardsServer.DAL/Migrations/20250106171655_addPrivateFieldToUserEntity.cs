f451033121961a9acf0d224028e71fe67f6aa28f
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2025-01-06 20:17:19 +0300

    feat: миграция (isPrivate для пользователя)

---
+++ CardsServer.DAL/Migrations/20250106171655_addPrivateFieldToUserEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CardsServer.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addPrivateFieldToUserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrivate",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPrivate",
+                table: "Users");
+        }
+    }
+}
