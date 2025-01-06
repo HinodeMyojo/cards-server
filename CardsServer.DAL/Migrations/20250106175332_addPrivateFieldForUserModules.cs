318b669d64a89388e9e01ea92f8d9160b9d8a54f
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2025-01-06 20:54:40 +0300

    feat: добавлена миграция поля "isPrivateForMe"

---
+++ CardsServer.DAL/Migrations/20250106175332_addPrivateFieldForUserModules.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CardsServer.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addPrivateFieldForUserModules : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrivateForMe",
+                table: "UserModules",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPrivateForMe",
+                table: "UserModules");
+        }
+    }
+}
