27c76b8a325784c60668e2ec4995c26f14cddf77
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2025-01-23 23:52:26 +0300

    feat: интерфейсы для стратегии

---
+++ CardsServer.BLL/Abstractions/Strategy/IModuleLoadingStrategy.cs
@@ -0,0 +1,10 @@
+﻿using CardsServer.BLL.Dto.Module;
+using CardsServer.BLL.Entity;
+
+namespace CardsServer.BLL.Abstractions
+{
+    public interface IModuleLoadingStrategy
+    {
+        IQueryable<ModuleEntity> ConfigureQuery(IQueryable<ModuleEntity> query, GetModules model);
+    }
+}
---
+++ CardsServer.BLL/Abstractions/Strategy/ISortStrategy.cs
@@ -0,0 +1,9 @@
+﻿using CardsServer.BLL.Entity;
+
+namespace CardsServer.BLL.Abstractions.Strategy
+{
+    public interface ISortStrategy
+    {
+        IQueryable<ModuleEntity> ApplySort(IQueryable<ModuleEntity> query);
+    }
+}
